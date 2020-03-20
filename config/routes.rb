64d573c189324495cd78a15230a677cc8a3c3a0c
Clor <clor.matthieu.tl@gmail.com>
2020-03-20 21:32:20 +0100

    create public layout with navbar and clean authentification

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   root to: 'public#index'
-  devise_for :users
+
+  devise_for :users, controllers: {
+    confirmations: 'authentication/user/confirmations',
+    passwords: 'authentication/user/passwords',
+    registrations: 'user_account/authentification/registrations',
+    sessions: 'user_account/authentification/sessions',
+    unlocks: 'authentication/user/unlocks'
+  }
 end
