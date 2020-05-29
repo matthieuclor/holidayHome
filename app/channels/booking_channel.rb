f699f6daf17adb450b75bda330697a72c9afef9c
Matthieu Clor <matthieuclor@gmail.com>
2020-05-29 09:52:44 +0200

    update rails and clean the code

--- app/channels/booking_channel.rb
+++ app/channels/booking_channel.rb
@@ -2,7 +2,4 @@ class BookingChannel < ApplicationCable::Channel
   def subscribed
     stream_from "booking_channel_#{params[:booking_id]}"
   end
-
-  def unsubscribed
-  end
 end
--- app/javascript/channels/booking_channel.js
+++ app/javascript/channels/booking_channel.js
@@ -9,10 +9,6 @@ document.addEventListener('turbolinks:load', () => {
     const bookingId = element.getAttribute('data-booking-id')
 
     consumer.subscriptions.create({ channel: "BookingChannel", booking_id: bookingId }, {
-      connected() {
-      },
-      disconnected() {
-      },
       received(data) {
         element.innerHTML = data['message'] + element.innerHTML
       }
