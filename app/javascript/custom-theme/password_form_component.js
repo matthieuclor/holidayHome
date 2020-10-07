ac1166a8890b16a55e8b07969632bf77d2b777a2
Matthieu Clor <matthieuclor@gmail.com>
2020-10-07 15:06:51 +0200

    clean the password-eye code

--- app/javascript/custom-theme/password_form_component.js
+++ app/javascript/custom-theme/password_form_component.js
@@ -1,18 +1,10 @@
 document.addEventListener('turbolinks:load', () => {
-  document.querySelectorAll(".password-eye").forEach((passwordEye) => {
-    passwordForm = passwordEye.previousElementSibling.querySelector('input.password')
+  document.querySelectorAll(".form-check.password-eye").forEach((passwordEye) => {
+    passwordForm = document.getElementById(passwordEye.getAttribute('data-target'))
 
     if (passwordForm) {
-      passwordEye.addEventListener('click', () => {
-        if (passwordForm.type == 'password') {
-          passwordForm.type = ''
-          passwordEye.classList.remove("fa-eye")
-          passwordEye.classList.add("fa-eye-slash")
-        } else {
-          passwordForm.type = 'password'
-          passwordEye.classList.remove("fa-eye-slash")
-          passwordEye.classList.add("fa-eye")
-        }
+      passwordEye.addEventListener('change', () => {
+        passwordForm.type = (passwordForm.type == 'password') ? '' : 'password'
       })
     }
   })
