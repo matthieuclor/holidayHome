783350cf0d063ba4b6cc01db1ca77e65ba756702
Matthieu Clor <matthieuclor@gmail.com>
2020-11-01 17:04:08 +0100

    designing Q&A public page

--- app/controllers/public/question_and_answers_controller.rb
+++ app/controllers/public/question_and_answers_controller.rb
@@ -3,7 +3,14 @@
 module Public
   class QuestionAndAnswersController < ApplicationController
     def index
-      @question_and_answers = QuestionAndAnswer.online
+      @query = QuestionAndAnswer.online.ransack(question_and_answer_ransack_params)
+      @question_and_answers = @query.result
+    end
+
+    private
+
+    def question_and_answer_ransack_params
+      @question_and_answer_ransack_params ||= params[:q]&.permit(:question_or_answer_cont)
     end
   end
 end
