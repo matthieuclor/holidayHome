b82d83eb03c2f4498cded0ce04b59c30b2a9fed1
Matthieu Clor <matthieuclor@gmail.com>
2020-11-11 19:52:35 +0100

    add some screenshot to finish main page

--- app/javascript/bookings/components/booking/booking_modal_form.vue
+++ app/javascript/bookings/components/booking/booking_modal_form.vue
@@ -3,7 +3,7 @@
     <div class="modal-dialog">
       <div v-if="bookingDateRange" class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title">Nouvelle déservation pour {{ currentVenue.name }}</h5>
+          <h5 class="modal-title">Nouvelle réservation pour {{ currentVenue.name }}</h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
