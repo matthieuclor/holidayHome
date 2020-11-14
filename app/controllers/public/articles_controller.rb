e6853b61f163ed9a0407c015b3602b91d97c42c7
Matthieu Clor <matthieuclor@gmail.com>
2020-11-14 23:45:50 +0100

    add search bar into blog section

--- app/controllers/public/articles_controller.rb
+++ app/controllers/public/articles_controller.rb
@@ -3,12 +3,19 @@
 module Public
   class ArticlesController < ApplicationController
     def index
-      @articles = Article.online
+      @query = Article.online.ransack(article_ransack_params)
+      @articles = @query.result
     end
 
     def show
       @article = Article.online.find_by(slug: params[:slug])
       head :not_found unless @article
+    end
+
+    private
+
+    def article_ransack_params
+      @article_ransack_params ||= params[:q]&.permit(:title_cont)
     end
   end
 end
