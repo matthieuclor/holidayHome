2aaa18da70e11aca40ecc6239fbe62c31b0f4f07
Matthieu Clor <matthieuclor@gmail.com>
2020-11-27 00:29:42 +0100

    remove x86-mingw32 from gemfile lock

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,6 +2,12 @@ const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
 
+environment.config.merge({
+  performance: {
+    hints: false
+  }
+})
+
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
 
