d82c43e21bb62b42d20f352f4f89915ea9cce081
Matthieu Clor <matthieuclor@gmail.com>
2020-12-11 22:25:49 +0100

    add meta og img type and size

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -12,4 +12,16 @@ module MetaTagsHelper
   def meta_image
     image_url(content_for?(:meta_image) || t('default_meta.meta_image'))
   end
+
+  def meta_image_type
+    content_for(:meta_image_type) || t('default_meta.meta_image_type')
+  end
+
+  def meta_image_width
+    content_for(:meta_image_width) || t('default_meta.meta_image_width')
+  end
+
+  def meta_image_height
+    content_for(:meta_image_height) || t('default_meta.meta_image_height')
+  end
 end
